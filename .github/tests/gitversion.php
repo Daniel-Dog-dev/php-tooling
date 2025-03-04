0e969a624abc83e88820aa964e2e7a85dc5f13aa
Daniel-Dog-dev <y33w4ayg@daniel-dog.com>
2025-03-04 18:37:50 +0100

    Added: GitVersion

---
+++ .github/tests/gitversion.php
@@ -0,0 +1,57 @@
+<?php
+
+    /*
+    MIT License
+
+    Copyright (c) 2025 Daniel-Dog-dev
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+    */
+
+    require(__DIR__ . "/../../autoload/autoload.php");
+
+    if(!file_exists(__DIR__ . "/gitinfo.json")){
+        echo "No gitinfo.json file found.";
+        exit(1);
+    }
+
+    $github_runner_gitinfo = json_decode(file_get_contents(__DIR__ . "/gitinfo.json"), true);
+    $gitversion = new GitVersion(__DIR__ . "/../../");
+
+    echo "Check if ref matches between GitVersion and gitinfo.json.\n";
+    if($gitversion->getRef() != $github_runner_gitinfo["ref"]){
+        echo "The Git ref of GitVersion did not match the gitinfo.json ref!\n";
+        echo "GitVersion: " . $gitversion->getRef() . "\n";
+        echo "gitinfo.json: " . $github_runner_gitinfo["ref"] . "\n";
+        exit(3);
+    }
+    echo "The ref matched between GitVersion and gitinfo.json.\n";
+
+    echo "Check if hash matches between GitVersion and gitinfo.json.\n";
+    if($gitversion->getHash() != $github_runner_gitinfo["hash"]){
+        echo "The Git hash of GitVersion did not match the gitinfo.json hash!\n";
+        echo "GitVersion: " . $gitversion->getHash() . "\n";
+        echo "gitinfo.json: " . $github_runner_gitinfo["hash"] . "\n";
+        exit(2);
+    }
+    echo "The hash matched between GitVersion and gitinfo.json.\n";
+
+    echo $gitversion->getVersion();
+    exit(0);
+?>
---
+++ modules/GitVersion/gitversion.php
@@ -0,0 +1,87 @@
+<?php
+
+	class GitVersion {
+		private $git_ref = null;
+		private $git_branch = null;
+		private $git_hash = null;
+		private $git_hash_short = null;
+		
+		/*
+		 * Class constructor.
+		 * @param $git_loc The folder where the .git folder is in.
+		 * @param $short_hash_length The short hash size. (Default: 7)
+		 * @return void
+		 */
+		public function __construct($git_loc, $short_hash_length = 7){
+			
+			if(!file_exists($git_loc)){
+				throw new Exception("Cannot find .git folder. Please make sure the .git folder is at " . __DIR__ . "/../.git/");
+			}
+			
+			$git_loc = realpath($git_loc);
+			
+			if(!is_integer($short_hash_length)){
+				throw new Exception("short_hash_length needs to be a integer.");
+			}
+			
+			$this->git_ref = implode("\n", array_slice(explode("\n", str_replace("ref: ", "", file_get_contents($git_loc . "/.git/HEAD"))), 0, 1));
+			$this->git_branch = preg_replace('/^.*\/\s*/', '', $this->git_ref);
+			$this->git_hash = implode("\n", array_slice(explode("\n", file_get_contents($git_loc . "/.git/" . $this->git_ref)), 0, 1));
+			
+			if($short_hash_length > strlen($this->git_hash)){
+				throw new Exception("short_hash_length cannot be larger then the hash itself.");
+			}
+			
+			$this->git_hash_short = substr($this->git_hash, 0, $short_hash_length);
+		}
+		
+		/*
+		 * Get the short hash value of the ref commit.
+		 * @return string.
+		 */
+		public function getShortHash(){
+			return $this->git_hash_short;
+		}
+		
+		/*
+		 * Get the hash value of the ref commit.
+		 * @return string
+		 */
+		public function getHash(){
+			return $this->git_hash;
+		}
+		
+		/*
+		 * Get the ref from the HEAD.
+		 */
+		public function getRef(){
+			return $this->git_ref;
+		}
+		
+		/*
+		 * Get the Git commit short hash and branch name.
+		 * @return string
+		 */
+		public function getVersion(){
+			return $this->getShortHash() . " (" . $this->git_branch . ")";
+		}
+		
+		/*
+		 * Set the size of the short hash.
+		 * @param $short_hash_length The short hash size. (Default: 7)
+		 * @return void
+		 */
+		public function setShortHashSize($short_hash_length = 7){
+			if(!is_integer($short_hash_length)){
+				throw new Exception("short_hash_length needs to be a integer.");
+			}
+			
+			if($short_hash_length > strlen($this->git_hash)){
+				throw new Exception("short_hash_length cannot be larger then the hash itself.");
+			}
+			
+			$this->git_hash_short = substr($this->git_hash, 0, $short_hash_length);
+		}
+	}
+
+?>
