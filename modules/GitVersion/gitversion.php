51a64e17ef47d2dda404cd692023611aaa9f62ad
Daniel-Dog-dev <y33w4ayg@daniel-dog.com>
2025-03-16 00:59:50 +0100

    Added: file version

--- modules/GitVersion/gitversion.php
+++ modules/GitVersion/gitversion.php
@@ -5,14 +5,18 @@
 		private $git_branch = null;
 		private $git_hash = null;
 		private $git_hash_short = null;
+		private $file_version = null;
 		
 		/*
 		 * Class constructor.
 		 * @param $git_loc The folder where the .git folder is in.
+		 * @param #git_loc_full Whether or not the $git_loc is a real path or a relative path.
 		 * @param $short_hash_length The short hash size. (Default: 7)
+		 * @param $file_version_loc The location of the version file.
+		 * @param $file_version_full_loc Whether or not the $file_version_loc is a real path or a relative path.
 		 * @return void
 		 */
-		public function __construct($git_loc, $git_loc_full = false, $short_hash_length = 7){
+		public function __construct($git_loc, $git_loc_full = false, $short_hash_length = 7, $file_version_loc = null, $file_version_full_loc = false){
 			
 			$git_loc = ($git_loc_full ? $git_loc : realpath($git_loc . "/.git/"));
 
@@ -33,6 +37,14 @@
 			}
 			
 			$this->git_hash_short = substr($this->git_hash, 0, $short_hash_length);
+
+			$file_version_loc = ($file_version_full_loc ? $file_version_loc : realpath($file_version_loc));
+
+			if(!file_exists($file_version_loc)){
+				throw new Exception("Cannot find version file. Please make sure the version file is at " . $file_version_loc);
+			}
+
+			$this->file_version = file_get_contents($file_version_loc);
 		}
 		
 		/*
@@ -70,7 +82,15 @@
 		 * @return string
 		 */
 		public function getVersion(){
-			return $this->getShortHash() . " (" . $this->git_branch . ")";
+			return ($this->file_version == null ? "" : $this->file_version . " ") . $this->getShortHash() . " (" . $this->git_branch . ")";
+		}
+
+		/*
+		 * Get the version of the version file.
+		 * @return string
+		 */
+		public function getFileVersion(){
+			return $this->file_version;
 		}
 		
 		/*
