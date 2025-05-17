5fb9a89a96ff5249901e7e79a557e19b7e3dfc25
Daniel-Dog-dev <y33w4ayg@daniel-dog.com>
2025-05-17 23:47:16 +0200

    Added: task for token cleanup

---
+++ tasks/token_cleanup.php
@@ -0,0 +1,12 @@
+<?php
+
+    if(!file_exists(__DIR__ . "/../../config/config.php")){
+        echo "Failed to get database config file.";
+        echo "Looked at location: " . __DIR__ . "/../../config/config.php";
+        exit(1);
+    }
+
+    $conn->query("DELETE FROM `users_tokens` WHERE `valid_till` < CURRENT_TIMESTAMP()");
+    exit(0);
+
+?>
